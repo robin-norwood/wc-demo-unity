2a02ad016e071945b3ddd5f55e2cf47403f12f90
Robin Norwood <robin.norwood@gmail.com>
2018-12-24 11:48:35 -0600

    Add button to make tokens

---
+++ Assets/Scripts/UI/GetToken.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetToken : MonoBehaviour
+{
+
+    public GameObject tokenPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GenerateToken() {
+        Vector3 pos = new Vector3(2,2,2); // FIXME...attach to mouse, or...?
+
+        GameObject token = (GameObject) Instantiate(this.tokenPrefab, pos, Quaternion.identity);
+        Debug.Log("Got a token!");
+    }
+}
