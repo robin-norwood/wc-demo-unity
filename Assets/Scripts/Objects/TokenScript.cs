e00b18895076ced8dee2cc8cb9705b0d6200e4ff
Robin Norwood <robin.norwood@gmail.com>
2018-12-26 13:53:57 -0600

    More tweaks to token behavior

--- Assets/Scripts/Objects/TokenScript.cs
+++ Assets/Scripts/Objects/TokenScript.cs
@@ -6,11 +6,16 @@ using System;
 public class TokenScript : MonoBehaviour
 {
     public bool followingMouse;
-    public float floatHeight = 1f;
+    public float floatHeight = 2f;
+
     public int boardLayer = 9;
     public int tokenLayer = 10;
     private int boardLayerMask;
     private int tokenLayerMask;
+
+    public Vector3 targetPosition;
+    public Vector3 velocity = Vector3.zero;
+    public float smoothTime = .01f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,8 @@ public class TokenScript : MonoBehaviour
         this.followingMouse = false;
         this.boardLayerMask = 1 << this.boardLayer;
         this.tokenLayerMask = 1 << this.tokenLayer;
+
+        this.targetPosition = this.gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -27,34 +34,32 @@ public class TokenScript : MonoBehaviour
         {
             RaycastHit hit;
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 targetPosition;
 
             if (Physics.Raycast(mouseRay, out hit, Mathf.Infinity, this.boardLayerMask))
             { // Above a hex tile: center on the tile and hover
-                targetPosition = hit.collider.gameObject.transform.position;
+                // TODO: Make sure token is "face up"
+                this.targetPosition = hit.collider.gameObject.transform.position;
+                targetPosition.y += this.floatHeight;
+                this.velocity = Vector3.zero;
             }
             else if (Physics.Raycast(mouseRay, out hit, Mathf.Infinity, ~this.tokenLayerMask))
             { // Above something else (ignore tokens): hover in place
-                targetPosition = hit.point;
-            }
-            else
-            { // Just follow the mouse
-                // FIXME: This isn't right...mousePosition isn't the same as world
-                // ...but in practice, I think the goal should be to have the table fill the entire view,
-                // so the mouse is always over it, and this code path never gets hit.
-                Debug.Log("MOUSE: " + Input.mousePosition);
-                targetPosition = Input.mousePosition;
+                this.targetPosition = hit.point;
+                targetPosition.y += this.floatHeight;
+                this.velocity = Vector3.zero;
             }
 
-            targetPosition.y += this.floatHeight;
-            this.gameObject.transform.position = targetPosition;
-            Debug.Log("TARGET: " + targetPosition);
+            if (this.gameObject.transform.position != this.targetPosition)
+            {
+                this.gameObject.transform.position = Vector3.SmoothDamp(this.gameObject.transform.position, this.targetPosition, ref this.velocity, this.smoothTime);
+            }
         }
     }
 
     void OnMouseDown()
     {
         // Pick up token
+        GetComponent<Rigidbody>().useGravity = false;
         this.followingMouse = true;
     }
 
@@ -62,7 +67,9 @@ public class TokenScript : MonoBehaviour
     {
         // Drop token
         // FIXME: Sometimes this glitches, and the token falls through the table, or goes to the wrong place.
-
+        // I think disabling rotation on the Token GO's RigidBody and expanding the table size "fixed" this.
+        GetComponent<Rigidbody>().useGravity = true;
+        this.gameObject.transform.position = this.targetPosition;
         this.followingMouse = false;
     }
 }
